fc8273f13e4e53bdf4c3cf36e8c5ec8edd19ef25
Eugene <Eugene@DESKTOP-LTEFHQB>
2024-02-18 22:41:16 +0200

    Add project files.

---
+++ ConsoleApp3/OpenClosedPrinciple.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public enum Size
+    {
+        small,
+        medium,
+        large
+    }
+
+    public enum Color
+    {
+        green,
+        blue,
+        red
+    }
+
+    public class Product
+    {
+        public readonly string Name;
+        public readonly Size Size;
+        public readonly Color Color;
+
+        public Product(string name, Size size, Color color)
+        {
+            Name = name;
+            Size = size;
+            Color = color;
+        }
+    }
+
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, Specification<T> spec);
+    }
+
+    public abstract class Specification<T>
+    {
+        public abstract bool IsSatisfied(T item);
+
+        public static Specification<T> operator &(Specification<T> left, Specification<T> right)
+        {
+            return new AndSpecifications<T>(left, right);
+        }
+    }
+
+    public class ColorSpecification : Specification<Product>
+    {
+        private readonly Color _color;
+
+        public ColorSpecification(Color color)
+        {
+            _color = color;
+        }
+
+        public override bool IsSatisfied(Product item)
+        {
+            if (item.Color == _color)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    public class SizeSpecification : Specification<Product>
+    {
+        private readonly Size _size;
+
+        public SizeSpecification(Size size)
+        {
+            _size = size;
+        }
+
+        public override bool IsSatisfied(Product item)
+        {
+            return item.Size == _size;
+        }
+    }
+
+    public class BetterFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> items, Specification<Product> spec)
+        {
+            foreach (Product item in items)
+            {
+                if (spec.IsSatisfied(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+
+    //combinator
+    public class AndSpecifications<T> : Specification<T>
+    {
+        private readonly Specification<T> _specification1;
+        private readonly Specification<T> _specification2;
+
+        public AndSpecifications(Specification<T> specification1, Specification<T> specification2)
+        {
+            _specification1 = specification1;
+            _specification2 = specification2;
+        }
+
+        public override bool IsSatisfied(T item)
+        {
+            return _specification1.IsSatisfied(item) && _specification2.IsSatisfied(item);
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
+using System.Net.Http.Headers;
+using static ConsoleApp3.Program;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            //openClosedPrincipe
+            var apple = new Product("Apple", Size.small, Color.green);
+            var tree = new Product("Tree", Size.large, Color.green);
+            var house = new Product("House", Size.large, Color.blue);
+            var product = new Product[] { apple, tree, house };
+            var bf = new BetterFilter();
+            var largeSpec = new SizeSpecification(Size.large);
+            var greenSpec = new ColorSpecification(Color.green);
+            var largeGreenSpec = new AndSpecifications<Product>(largeSpec, greenSpec);
+
+
+            foreach (var item in bf.Filter(product, largeSpec))
+            {
+                Console.WriteLine(item.Name + item.Size);
+            }
+
+            foreach (var item in bf.Filter(product, greenSpec))
+            {
+                Console.WriteLine(item.Name + item.Color);
+            }
+
+            //use kombinator for operators
+            foreach (var item in bf.Filter(product, largeGreenSpec))
+            {
+                Console.WriteLine(item.Name + item.Color + item.Size);
+            }
+
+            //use operator
+            foreach (var item in bf.Filter(product, largeSpec & greenSpec))
+            {
+                Console.WriteLine(item.Name + item.Size + item.Color);
+            }
+        }
+
+
+    }
+}
